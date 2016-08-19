dd8ccd805322affff984045946155f090b6639fc
SNR <xvanok@nag.ru>
2016-08-19 12:07:34 +0500

    Tables creating bug fix

--- insert_tables.php
+++ insert_tables.php
@@ -26,14 +26,93 @@ try {
     echo "Текст ошибки: ".$e;
     die();
 }
-try {
-    $mysqli->query("CREATE TABLE IF NOT EXISTS `vermax_configs` (`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, `name` VARCHAR( 255 ) NULL , `descript` TEXT NULL, PRIMARY KEY ( `id` )) ENGINE = INNODB");
-    $mysqli->query("CREATE TABLE IF NOT EXISTS `vermax_conf_params` (`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, `conf_id` INT NULL, `name` VARCHAR( 255 ) NULL, `value` VARCHAR( 255 ) NULL, `comment` TEXT NULL, PRIMARY KEY ( `id` )) ENGINE = INNODB");
-    $mysqli->query("CREATE TABLE IF NOT EXISTS `vermax_firmware_params` (`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, `conf_id` INT NULL, `name` VARCHAR( 255 ) NULL, `value` VARCHAR( 255 ) NULL, `comment` TEXT NULL, PRIMARY KEY ( `id` )) ENGINE = INNODB");
-    $mysqli->query("CREATE TABLE IF NOT EXISTS `vermax_setting_params` (`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, `conf_id` INT NULL, `name` VARCHAR( 255 ) NULL, `value` VARCHAR( 255 ) NULL, `comment` TEXT NULL, PRIMARY KEY ( `id` )) ENGINE = INNODB");
-    $mysqli->query("CREATE TABLE IF NOT EXISTS `vermax_update_params` (`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, `conf_id` INT NULL, `name` VARCHAR( 255 ) NULL, `value` VARCHAR( 255 ) NULL, `comment` TEXT NULL, PRIMARY KEY ( `id` )) ENGINE = INNODB");
-    $mysqli->query("CREATE TABLE IF NOT EXISTS `vermax_conf_user` (`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, `ip` VARCHAR( 50 ) NULL, `mac` VARCHAR ( 255 ) NULL, `conf_id` INT NULL, `upd_id` INT NULL, `settings_id` INT NULL, `fw_id` INT NULL, PRIMARY KEY ( `id` )) ENGINE = INNODB");
-} catch(Exception $e){
-    echo "Не удалось создать таблицы: ".$e;
-    die();
+
+$mysqli->query("CREATE TABLE IF NOT EXISTS `vermax_configs` (`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, `name` VARCHAR( 255 ) NULL , `descript` TEXT NULL) ENGINE = INNODB");
+if($mysqli->errno){
+	echo "При создании таблицы возникла ошибка:\r\n\r\n########## SYSTEM INFO ##########\r\n\r\n".php_uname()."\r\n\r\n########## MySQLi INFO ##########\r\n\r\n".$mysqli->server_info."\r\n\r\n############# ERROR #############\r\n\r\n".$mysqli->errno.": ".$mysqli->error."\r\n\r\n#################################\r\n\r\nПовторная попытка создания таблицы.\r\n\r\n";
+	$mysqli->query("CREATE TABLE IF NOT EXISTS `vermax_configs` (`id` INT NOT NULL AUTO_INCREMENT, `name` VARCHAR( 255 ) NULL , `descript` TEXT NULL, PRIMARY KEY ( `id` )) ENGINE = INNODB");
+	if($mysqli->errno){
+		echo "При создании таблицы возникла ошибка:\r\n\r\n########## SYSTEM INFO ##########\r\n\r\n".php_uname()."\r\n\r\n########## MySQLi INFO ##########\r\n\r\n".$mysqli->server_info."\r\n\r\n############# ERROR #############\r\n\r\n".$mysqli->errno.": ".$mysqli->error."\r\n\r\n#################################\r\n\r\n";
+	}
+	else{
+		echo "\r\nТаблица `vermax_configs` успешно создана!\r\n\r\n";
+	}
+}
+else{
+	echo "\r\nТаблица `vermax_configs` успешно создана!\r\n\r\n";
+}
+
+$mysqli->query("CREATE TABLE IF NOT EXISTS `vermax_conf_params` (`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, `conf_id` INT NULL, `name` VARCHAR( 255 ) NULL, `value` VARCHAR( 255 ) NULL, `comment` TEXT NULL) ENGINE = INNODB");
+if($mysqli->errno){
+	echo "При создании таблицы возникла ошибка:\r\n\r\n########## SYSTEM INFO ##########\r\n\r\n".php_uname()."\r\n\r\n########## MySQLi INFO ##########\r\n\r\n".$mysqli->server_info."\r\n\r\n############# ERROR #############\r\n\r\n".$mysqli->errno.": ".$mysqli->error."\r\n\r\n#################################\r\n\r\nПовторная попытка создания таблицы.\r\n\r\n";
+	$mysqli->query("CREATE TABLE IF NOT EXISTS `vermax_conf_params` (`id` INT NOT NULL AUTO_INCREMENT, `conf_id` INT NULL, `name` VARCHAR( 255 ) NULL, `value` VARCHAR( 255 ) NULL, `comment` TEXT NULL, PRIMARY KEY ( `id` )) ENGINE = INNODB");
+	if($mysqli->errno){
+		echo "При создании таблицы возникла ошибка:\r\n\r\n########## SYSTEM INFO ##########\r\n\r\n".php_uname()."\r\n\r\n########## MySQLi INFO ##########\r\n\r\n".$mysqli->server_info."\r\n\r\n############# ERROR #############\r\n\r\n".$mysqli->errno.": ".$mysqli->error."\r\n\r\n#################################\r\n\r\n";
+	}
+	else{
+		echo "\r\nТаблица `vermax_conf_params` успешно создана!\r\n\r\n";
+	}
+}
+else{
+	echo "\r\nТаблица `vermax_conf_params` успешно создана!\r\n\r\n";
+}
+
+$mysqli->query("CREATE TABLE IF NOT EXISTS `vermax_firmware_params` (`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, `conf_id` INT NULL, `name` VARCHAR( 255 ) NULL, `value` VARCHAR( 255 ) NULL, `comment` TEXT NULL) ENGINE = INNODB");
+if($mysqli->errno){
+	echo "При создании таблицы возникла ошибка:\r\n\r\n########## SYSTEM INFO ##########\r\n\r\n".php_uname()."\r\n\r\n########## MySQLi INFO ##########\r\n\r\n".$mysqli->server_info."\r\n\r\n############# ERROR #############\r\n\r\n".$mysqli->errno.": ".$mysqli->error."\r\n\r\n#################################\r\n\r\nПовторная попытка создания таблицы.\r\n\r\n";
+	$mysqli->query("CREATE TABLE IF NOT EXISTS `vermax_firmware_params` (`id` INT NOT NULL AUTO_INCREMENT, `conf_id` INT NULL, `name` VARCHAR( 255 ) NULL, `value` VARCHAR( 255 ) NULL, `comment` TEXT NULL, PRIMARY KEY ( `id` )) ENGINE = INNODB");
+	if($mysqli->errno){
+		echo "При создании таблицы возникла ошибка:\r\n\r\n########## SYSTEM INFO ##########\r\n\r\n".php_uname()."\r\n\r\n########## MySQLi INFO ##########\r\n\r\n".$mysqli->server_info."\r\n\r\n############# ERROR #############\r\n\r\n".$mysqli->errno.": ".$mysqli->error."\r\n\r\n#################################\r\n\r\n";
+	}
+	else{
+		echo "\r\nТаблица `vermax_firmware_params` успешно создана!\r\n\r\n";
+	}
+}
+else{
+	echo "\r\nТаблица `vermax_firmware_params` успешно создана!\r\n\r\n";
+}
+
+$mysqli->query("CREATE TABLE IF NOT EXISTS `vermax_setting_params` (`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, `conf_id` INT NULL, `name` VARCHAR( 255 ) NULL, `value` VARCHAR( 255 ) NULL, `comment` TEXT NULL) ENGINE = INNODB");
+if($mysqli->errno){
+	echo "При создании таблицы возникла ошибка:\r\n\r\n########## SYSTEM INFO ##########\r\n\r\n".php_uname()."\r\n\r\n########## MySQLi INFO ##########\r\n\r\n".$mysqli->server_info."\r\n\r\n############# ERROR #############\r\n\r\n".$mysqli->errno.": ".$mysqli->error."\r\n\r\n#################################\r\n\r\nПовторная попытка создания таблицы.\r\n\r\n";
+	$mysqli->query("CREATE TABLE IF NOT EXISTS `vermax_setting_params` (`id` INT NOT NULL AUTO_INCREMENT, `conf_id` INT NULL, `name` VARCHAR( 255 ) NULL, `value` VARCHAR( 255 ) NULL, `comment` TEXT NULL, PRIMARY KEY ( `id` )) ENGINE = INNODB");
+	if($mysqli->errno){
+		echo "При создании таблицы возникла ошибка:\r\n\r\n########## SYSTEM INFO ##########\r\n\r\n".php_uname()."\r\n\r\n########## MySQLi INFO ##########\r\n\r\n".$mysqli->server_info."\r\n\r\n############# ERROR #############\r\n\r\n".$mysqli->errno.": ".$mysqli->error."\r\n\r\n#################################\r\n\r\n";
+	}
+	else{
+		echo "\r\nТаблица `vermax_setting_params` успешно создана!\r\n\r\n";
+	}
+}
+else{
+	echo "\r\nТаблица `vermax_setting_params` успешно создана!\r\n\r\n";
+}
+
+$mysqli->query("CREATE TABLE IF NOT EXISTS `vermax_update_params` (`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, `conf_id` INT NULL, `name` VARCHAR( 255 ) NULL, `value` VARCHAR( 255 ) NULL, `comment` TEXT NULL) ENGINE = INNODB");
+if($mysqli->errno){
+	echo "При создании таблицы возникла ошибка:\r\n\r\n########## SYSTEM INFO ##########\r\n\r\n".php_uname()."\r\n\r\n########## MySQLi INFO ##########\r\n\r\n".$mysqli->server_info."\r\n\r\n############# ERROR #############\r\n\r\n".$mysqli->errno.": ".$mysqli->error."\r\n\r\n#################################\r\n\r\nПовторная попытка создания таблицы.\r\n\r\n";
+	$mysqli->query("CREATE TABLE IF NOT EXISTS `vermax_update_params` (`id` INT NOT NULL AUTO_INCREMENT, `conf_id` INT NULL, `name` VARCHAR( 255 ) NULL, `value` VARCHAR( 255 ) NULL, `comment` TEXT NULL, PRIMARY KEY ( `id` )) ENGINE = INNODB");
+	if($mysqli->errno){
+		echo "При создании таблицы возникла ошибка:\r\n\r\n########## SYSTEM INFO ##########\r\n\r\n".php_uname()."\r\n\r\n########## MySQLi INFO ##########\r\n\r\n".$mysqli->server_info."\r\n\r\n############# ERROR #############\r\n\r\n".$mysqli->errno.": ".$mysqli->error."\r\n\r\n#################################\r\n\r\n";
+	}
+	else{
+		echo "\r\nТаблица `vermax_update_params` успешно создана!\r\n\r\n";
+	}
+}
+else{
+	echo "\r\nТаблица `vermax_update_params` успешно создана!\r\n\r\n";
+}
+
+$mysqli->query("CREATE TABLE IF NOT EXISTS `vermax_conf_user` (`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, `ip` VARCHAR( 50 ) NULL, `mac` VARCHAR ( 255 ) NULL, `conf_id` INT NULL, `upd_id` INT NULL, `settings_id` INT NULL, `fw_id` INT NULL) ENGINE = INNODB");
+if($mysqli->errno){
+	echo "При создании таблицы возникла ошибка:\r\n\r\n########## SYSTEM INFO ##########\r\n\r\n".php_uname()."\r\n\r\n########## MySQLi INFO ##########\r\n\r\n".$mysqli->server_info."\r\n\r\n############# ERROR #############\r\n\r\n".$mysqli->errno.": ".$mysqli->error."\r\n\r\n#################################\r\n\r\nПовторная попытка создания таблицы.\r\n\r\n";
+	$mysqli->query("CREATE TABLE IF NOT EXISTS `vermax_conf_user` (`id` INT NOT NULL AUTO_INCREMENT, `ip` VARCHAR( 50 ) NULL, `mac` VARCHAR ( 255 ) NULL, `conf_id` INT NULL, `upd_id` INT NULL, `settings_id` INT NULL, `fw_id` INT NULL, PRIMARY KEY ( `id` )) ENGINE = INNODB");
+	if($mysqli->errno){
+		echo "При создании таблицы возникла ошибка:\r\n\r\n########## SYSTEM INFO ##########\r\n\r\n".php_uname()."\r\n\r\n########## MySQLi INFO ##########\r\n\r\n".$mysqli->server_info."\r\n\r\n############# ERROR #############\r\n\r\n".$mysqli->errno.": ".$mysqli->error."\r\n\r\n#################################\r\n\r\n";
+	}
+	else{
+		echo "\r\nТаблица `vermax_conf_user` успешно создана!\r\n\r\n";
+	}
+}
+else{
+	echo "\r\nТаблица `vermax_conf_user` успешно создана!\r\n\r\n";
 }
